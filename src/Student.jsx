3323d5f0b8a226efb4332a577499c608c73eef99
RitikaSingh23 <ritika2302singh@gmail.com>
2025-03-29 17:03:32 +0530

    add

--- src/Student.jsx
+++ src/Student.jsx
@@ -3,9 +3,9 @@ const Student=(props)=>{
     return(
         <><p>My name is {props.name}</p>
         <p>My city is {props.city}</p>
-        <p>My subject is{props.sub}</p>
+        <p>My subject is {props.sub}</p>
         </>
     )
 
 }
-export default Student
+export default Student;
