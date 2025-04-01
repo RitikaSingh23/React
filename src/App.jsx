3ebdba4d139061ddcc2a72ca36ed94095ff58209
RitikaSingh23 <ritika2302singh@gmail.com>
2025-04-01 17:25:10 +0530

    ReactRouterDom

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ const App=()=>{
         <Route path="home" element={<Home/>}/>
         <Route path="about" element={<About/>}/>
         <Route path="contact" element={<Contact/>}/>
-
       </Route>
     </Routes>
     </BrowserRouter>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,7 @@
 const Contact=()=>{
     return(
         <>
-        <h1> contact page</h1>
+        <h1> Contact page</h1>
         </>
     )
 }
